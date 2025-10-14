072db1109f14698f1c986202ed8166ea56e22710
Nabeela33 <41548534+Nabeela33@users.noreply.github.com>
2025-10-14 16:19:29 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,20 @@
+from google.cloud import storage
+import pandas as pd
 
+def load_mapping(bucket_name, file_name):
+    """
+    Load mapping file (Excel or CSV) from GCS
+    """
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+    data_bytes = blob.download_as_bytes()
+    
+    if file_name.lower().endswith(".xlsx") or file_name.lower().endswith(".xls"):
+        df = pd.read_excel(data_bytes)
+    elif file_name.lower().endswith(".csv"):
+        df = pd.read_csv(pd.io.common.BytesIO(data_bytes))
+    else:
+        raise ValueError("Unsupported mapping file format")
+    
+    return df
