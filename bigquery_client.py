298f5015563c76f1d28d4be07896f52f4574024e
Nabeela33 <41548534+Nabeela33@users.noreply.github.com>
2025-10-16 18:23:42 +0100

    Update bigquery_client.py

--- bigquery_client.py
+++ bigquery_client.py
@@ -2,8 +2,15 @@ from google.cloud import bigquery
 
 class BigQueryAgent:
     def __init__(self, project_id):
+        self.project_id = project_id
         self.client = bigquery.Client(project=project_id)
-    
+
+    def list_datasets(self):
+        """Return list of dataset names in project"""
+        return [ds.dataset_id for ds in self.client.list_datasets()]
+
     def execute(self, sql_query):
+        """Execute SQL in BigQuery and return pandas DataFrame"""
         query_job = self.client.query(sql_query)
-        return query_job.result().to_dataframe()
+        results = query_job.result()
+        return results.to_dataframe()
