57e88b1e64a96ce0b5c81a74130e1dfdf3fb6e83
Nabeela33 <41548534+Nabeela33@users.noreply.github.com>
2025-10-17 17:37:47 +0100

    Update bigquery_client.py

--- bigquery_client.py
+++ bigquery_client.py
@@ -1,10 +1,11 @@
 from google.cloud import bigquery
+import pandas as pd
 
 class BigQueryAgent:
-    def __init__(self, project_id):
+    def __init__(self, project_id: str):
         self.client = bigquery.Client(project=project_id)
 
-    def execute(self, query):
-        """Execute SQL and return DataFrame."""
-        job = self.client.query(query)
-        return job.result().to_dataframe()
+    def execute(self, query: str) -> pd.DataFrame:
+        query_job = self.client.query(query)
+        df = query_job.result().to_dataframe()
+        return df
