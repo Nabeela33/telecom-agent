3c4e79cfd9d3b4b414d13ae8f751955f5c09ef3f
Nabeela33 <41548534+Nabeela33@users.noreply.github.com>
2025-11-03 18:12:14 +0000

    Update bigquery_client.py

--- bigquery_client.py
+++ bigquery_client.py
@@ -8,3 +8,7 @@ class BigQueryAgent:
     def execute(self, query: str) -> pd.DataFrame:
         job = self.client.query(query)
         return job.result().to_dataframe()
+
+    def execute_with_config(self, query: str, job_config=None) -> pd.DataFrame:
+        job = self.client.query(query, job_config=job_config)
+        return job.result().to_dataframe()
