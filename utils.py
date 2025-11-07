9a6e345e8594ff3eb55ea96ce4d9d23c01477c98
Nabeela33 <41548534+Nabeela33@users.noreply.github.com>
2025-11-07 18:45:14 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,14 +1,30 @@
+import yaml
 from google.cloud import storage
 
 def load_mapping(bucket_name: str, file_name: str) -> str:
-    """Load text mapping file from GCS."""
     if "/" in bucket_name:
         bucket_name, prefix = bucket_name.split("/", 1)
         blob_name = f"{prefix}/{file_name}"
     else:
         blob_name = file_name
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
+    client = storage.Client()
+    bucket = client.bucket(bucket_name)
     blob = bucket.blob(blob_name)
     return blob.download_as_text()
+
+def load_yaml_config(bucket_name, file_path):
+    if bucket_name:
+        client = storage.Client()
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(file_path)
+        return yaml.safe_load(blob.download_as_text())
+    else:
+        with open(file_path, "r") as f:
+            return yaml.safe_load(f)
+
+def get_control_config(control_type, product_name, config_data):
+    try:
+        control_section = config_data["controls"].get(control_type, {})
+        return control_section.get(product_name, {})
+    except Exception as e:
+        raise ValueError(f"Invalid configuration: {e}")
