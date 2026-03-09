c4333a9c9fa3fadc57b82def676debf3b7a8d7cf
Nabeela33 <41548534+Nabeela33@users.noreply.github.com>
2026-03-09 12:23:01 +0000

    Update data_loader.py

--- systems/data_loader.py
+++ systems/data_loader.py
@@ -7,11 +7,11 @@ def fetch_system_data(project_id, systems):
 
     for system in systems:
         if system.lower() == "siebel":
-            system_dfs["siebel_siebel_accounts"] = bq.execute("SELECT * FROM `telecom-data-lake.o_siebel.siebel_accounts`")
-            system_dfs["siebel_siebel_assets"] = bq.execute("SELECT * FROM `telecom-data-lake.o_siebel.siebel_assets`")
-            system_dfs["siebel_siebel_orders"] = bq.execute("SELECT * FROM `telecom-data-lake.o_siebel.siebel_orders`")
+            system_dfs["siebel_accounts"] = bq.execute("SELECT * FROM `telecom-data-lake.o_siebel.siebel_accounts`")
+            system_dfs["siebel_assets"] = bq.execute("SELECT * FROM `telecom-data-lake.o_siebel.siebel_assets`")
+            system_dfs["siebel_orders"] = bq.execute("SELECT * FROM `telecom-data-lake.o_siebel.siebel_orders`")
         elif system.lower() == "antillia":
-            system_dfs["antillia_billing_accounts"] = bq.execute("SELECT * FROM `telecom-data-lake.gibantillia.billing_accounts`")
-            system_dfs["antillia_billing_products"] = bq.execute("SELECT * FROM `telecom-data-lake.gibantillia.billing_products`")
+            system_dfs["billing_accounts"] = bq.execute("SELECT * FROM `telecom-data-lake.gibantillia.billing_accounts`")
+            system_dfs["billing_products"] = bq.execute("SELECT * FROM `telecom-data-lake.gibantillia.billing_products`")
 
     return system_dfs
